3f1440909e0281d97c023b248448ebaa88f7a72f
Emad Ali <mc.lilzee@hotmail.com>
2022-11-03 10:14:55 +0100

    Fix binary search bug quicking too soon

--- src/main/java/project/searchable/BinarySearcher.java
+++ src/main/java/project/searchable/BinarySearcher.java
@@ -12,6 +12,28 @@ public class BinarySearcher<T extends Comparable<T>> extends Searcher<T> {
 
     @Override
     boolean isElementInSearchableFile(T element) {
+        int startIndex = 0;
+        int endIndex = searchableContent.length - 1;
+        int middleIndex = (startIndex + endIndex) / 2;
+
+        T compareElement;
+
+        while (startIndex <= endIndex && endIndex >= startIndex) {
+            compareElement = searchableContent[middleIndex];
+
+            if (element.compareTo(compareElement) > 0) {
+                startIndex = middleIndex + 1;
+                middleIndex = (startIndex + endIndex) / 2;
+            } else if (element.compareTo(compareElement) < 0) {
+                endIndex = middleIndex - 1;
+                middleIndex = (startIndex + endIndex) / 2;
+            }
+
+            if (element.compareTo(compareElement) == 0) {
+                return true;
+            }
+        }
+
         return false;
     }
 }
