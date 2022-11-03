ff528ee8638f803d1d1d280cdc6de18d9cd2553c
Emad Ali <mc.lilzee@hotmail.com>
2022-11-03 08:56:25 +0100

    Add quicksort algorithm

--- src/main/java/project/searchable/QuickSorter.java
+++ src/main/java/project/searchable/QuickSorter.java
@@ -1,7 +1,9 @@
 package project.searchable;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class QuickSorter<T extends Comparable<T>> extends Sorter<T> {
 
@@ -10,6 +12,37 @@ public class QuickSorter<T extends Comparable<T>> extends Sorter<T> {
     }
 
     @Override
-    void startSorting(T[] unsortedArray) {
+    void startSorting(T[] content) {
+        List<T> contentList = List.of(content);
+        quickSortList(contentList).toArray(content);
+    }
+
+    private List<T> quickSortList(List<T> contentList) {
+        if (contentList.size() <= 1) {
+            return contentList;
+        }
+
+        int pivotIndex = contentList.size() / 2;
+        List<T> smaller = new ArrayList<>();
+        List<T> larger = new ArrayList<>();
+        T pivot = contentList.get(pivotIndex);
+
+        for (int i = 0; i < contentList.size(); i++) {
+            if (i == pivotIndex) {
+                continue;
+            }
+
+            if (contentList.get(i).compareTo(pivot) < 0) {
+                smaller.add(contentList.get(i));
+            } else {
+                larger.add(contentList.get(i));
+            }
+        }
+
+        List<T> combine = new ArrayList<>(quickSortList(smaller));
+        combine.add(pivot);
+        combine.addAll(quickSortList(larger));
+
+        return combine;
     }
 }
