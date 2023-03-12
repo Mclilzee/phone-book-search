ddc414209118baeba4ed144244a623b476b5db74
mclilzee <mc.lilzee@hotmail.com>
2023-03-12 19:53:58 +0100

    Adjust program to use args for searching

--- src/main/java/project/Main.java
+++ src/main/java/project/Main.java
@@ -3,12 +3,21 @@ package project;
 import project.searchable.*;
 
 public class Main {
-    private static final Contact[] SEARCHABLE_CONTACTS = ContactReader.readFileToContactArray("./phonebook.txt");
-    private static final Contact[] TO_FIND = ContactReader.readFileToContactArray("./find.txt");
 
     public static void main(String[] args) {
+        if (args.length < 2) {
+          System.out.println("Need to provide path to searchable contact, and contact to search");
+          System.exit(-1);
+        }
+
+        Contact[] searchableContacts = ContactReader.readFileToContactArray("./phonebook.txt");
+        Contact[] toFind = ContactReader.readFileToContactArray("./find.txt");
+        processSearching(searchableContacts, toFind);
+    }
+
+    public static void processSearching(Contact[] searchableContacts, Contact[] toFind) {
         System.out.println("Start searching (linear search)...");
-        LinearSearcher<Contact> linearSearcher = new LinearSearcher<>(SEARCHABLE_CONTACTS, TO_FIND);
+        LinearSearcher<Contact> linearSearcher = new LinearSearcher<>(searchableContacts, toFind);
         System.out.println(linearSearcher.search());
         System.out.println();
 
@@ -16,22 +25,23 @@ public class Main {
         Sorter<Contact> quickSorter = new QuickSorter<>(linearSearcher.getSearchDuration().multipliedBy(10));
 
         System.out.println("Start searching (bubble sort + jump search)...");
-        JumpSearcher<Contact> jumpSearcher = new JumpSearcher<>(SEARCHABLE_CONTACTS, TO_FIND, bubbleSorter);
+        JumpSearcher<Contact> jumpSearcher = new JumpSearcher<>(searchableContacts, toFind, bubbleSorter);
         System.out.println(jumpSearcher.search());
         System.out.println();
 
         System.out.println("Start searching (quick sort + jump search)...");
-        JumpSearcher<Contact> jumpSearchQuickSort = new JumpSearcher<>(SEARCHABLE_CONTACTS, TO_FIND, quickSorter);
+        JumpSearcher<Contact> jumpSearchQuickSort = new JumpSearcher<>(searchableContacts, toFind, quickSorter);
         System.out.println(jumpSearchQuickSort.search());
         System.out.println();
 
         System.out.println("Start searching (quick sort + binary search)...");
-        BinarySearcher<Contact> binarySearcher = new BinarySearcher<>(SEARCHABLE_CONTACTS, TO_FIND, quickSorter);
+        BinarySearcher<Contact> binarySearcher = new BinarySearcher<>(searchableContacts, toFind, quickSorter);
         System.out.println(binarySearcher.search());
         System.out.println();
 
         System.out.println("Start searching (hash table search)...");
-        ContactHashSearch hashSearch = new ContactHashSearch(SEARCHABLE_CONTACTS, TO_FIND);
+        ContactHashSearch hashSearch = new ContactHashSearch(searchableContacts, toFind);
         System.out.println(hashSearch.search());
+
     }
 }
